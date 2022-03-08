0bf2ad806ac9e4781da582a2c124c5776f5cbee7
ksander <info@ksander.li>
2022-03-08 17:43:58 +0200

    frontpage

---
+++ frontend/src/components/login.js
@@ -0,0 +1,24 @@
+import React, {useEffect, useRef, useState, useMemo} from "react";
+import {Controller, useForm} from 'react-hook-form'
+import axios from 'axios';
+import TextField from '@mui/material'
+
+export default function Login(){
+    const {handleSubmit, control} = useForm()
+    
+
+
+    return (
+        <div className="container">
+            <div className="login-box">
+                <h1>Title</h1>
+            </div>
+        </div>
+
+
+
+
+
+
+    )
+}
