bacedf60a527fc7f47fcb4eb5d6940e430b9abbe
ksander <info@ksander.li>
2022-03-09 13:52:19 +0200

    react routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,23 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import {Route, useHistory} from 'react-router-dom'
 
 import Login from './components/login'
+import Register from './components/register'
 
 
 const App = () => {
+    const history = useHistory();
+
+  useEffect(() => {
+    history.push("/login")
+  })
+
     return (
-        <div className='App'>
-            <Login/>
+        <div className="container">
+             <Route path="/login" component={Login}/>
+             <Route path="/register" component={Register}/>
         </div>
+        
 
 
        
