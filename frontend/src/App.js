d6fb9eaf1d984fcfb68c3317a8f7efc522b98aa0
alexanl <aleksandr.liski@metropola.fi>
2022-03-10 03:54:31 +0200

    239r8j2398jr3298

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React, {Fragment, useEffect} from 'react'
-import {Routes, Route, Redirect, Link, useNavigate } from 'react-router-dom'
+import {Routes, Route, Navigate, Link, useNavigate } from 'react-router-dom'
 
 import { getDecodedToken } from './api/token'
 
@@ -12,6 +12,7 @@ import NotFound from "./components/notFound";
 
 const App = () => {
     const navigate = useNavigate();
+    
     const state = {
         decodedToken: getDecodedToken(), // hakee kirjautumisavaimen localStorage API:sta, jos se on tyhjä palauttaa null
     }
@@ -20,17 +21,17 @@ const App = () => {
 
 
     const requireAuth = ({component}) => {
-        return loggedIn ? component : <navigate to="login" />
+        return loggedIn ? component : <Navigate to="login" />
     }
 
 
     return (
         <div className="App">
             <Routes>
-                <Route exact path="/" element={ requireAuth(<Home />) } />
-                <Route path="/login" element={<Login />}/>
+                <Route exact path="/" element={ <requireAuth> <Home /> </requireAuth> } />
+                <Route path="login" element={<Login />}/>
                 <Route path="register" element={<Register />}/>
-                <Route element={<NotFound />}/>
+                <Route path="*" element={<NotFound />}/>
             </Routes>
         </div>
     )
