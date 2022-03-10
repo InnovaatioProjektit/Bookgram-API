66f8c10d673c7f5b5ffc1bffb534d252839793ef
alexanl <aleksandr.liski@metropola.fi>
2022-03-10 23:32:08 +0200

    dfgj8iejt9ew

--- frontend/src/components/register.js
+++ frontend/src/components/register.js
@@ -78,7 +78,7 @@ export default function Register(){
     const validateEmail = (e) => {
         // regex from http://stackoverflow.com/questions/46155/validate-email-address-in-javascript
         var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        return re.test(e) && isRequired(e)
+        return re.test(e) || isRequired(e)
     }
 
 
