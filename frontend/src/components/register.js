d178218e1ecb3e1743b79f1ae950e58d6976be7f
alexanl <aleksandr.liski@metropola.fi>
2022-03-10 23:38:12 +0200

    rgjiuehg348

--- frontend/src/components/register.js
+++ frontend/src/components/register.js
@@ -79,7 +79,7 @@ export default function Register(){
     const validateEmail = (e) => {
         // regex from http://stackoverflow.com/questions/46155/validate-email-address-in-javascript
         var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        return re.test(e) || isRequired(e)
+        return re.test(e) && isRequired(e)
     }
 
 
@@ -170,7 +170,6 @@ export default function Register(){
                   </Grid>
               </Grid>
               <Button
-                disabled = {err || !pwdMatch || !UserErr || !SurErr}
                 type="submit"
                 fullWidth
                 variant="contained"
