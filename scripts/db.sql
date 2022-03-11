c7228630126771c00c03a70a927c67c18d1149e4
alexanl <aleksandr.liski@metropola.fi>
2022-03-11 03:03:17 +0200

    eroigjeriouh49

--- scripts/db.sql
+++ scripts/db.sql
@@ -7,6 +7,8 @@ CREATE TABLE IF NOT EXISTS userSchema.User(
     id SERIAL NOT NULL,
     username VARCHAR(45) NOT NULL,
     passwd VARCHAR(60) NOT NULL,
+    name VARCHAR(25),
+    lastname VARCHAR(25)
 
     PRIMARY KEY (id)
 );
